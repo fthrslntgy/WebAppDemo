ef1b4d6455f6701d53afd7cdf31c79b1cdf6aa1f
Fatih <ftugay@etu.edu.tr>
2021-03-14 18:59:41 +0300

    Updates for Heroku!

--- src/main/java/myDemoApp/App.java
+++ src/main/java/myDemoApp/App.java
@@ -3,6 +3,15 @@
  */
 package myDemoApp;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 public class App {
 
     public static boolean checkIsBetweenBounds(int[] array, Integer index, int lowerBound, int upperBound){
@@ -13,4 +22,55 @@ public class App {
         
         return (array[index] <= upperBound && array[index] >= lowerBound);
     }
+
+    public static void main(String[] args) {
+        
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "Welcome to BIL481 HW1 Part 2 Web Application!");
+        post("/compute", (req, res) -> {
+
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+            }
+            sc1.close();
+            int[] inputListAsArray = new int[inputList.size()];
+            for(int i = 0; i < inputList.size(); i++)
+                inputListAsArray[i] = inputList.get(i);
+
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+            String input3 = req.queryParams("input3").replaceAll("\\s","");
+            int input3AsInt = Integer.parseInt(input3);
+            String input4 = req.queryParams("input4").replaceAll("\\s","");
+            int input4AsInt = Integer.parseInt(input4);
+
+            boolean result = App.checkIsBetweenBounds(inputListAsArray, input2AsInt, input3AsInt, input4AsInt);
+            Map<String, Boolean> map = new HashMap<String, Boolean>();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+        get("/compute", (rq, rs) -> {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("result", "not computed yet!");
+            return new ModelAndView(map, "compute.mustache");
+        },
+        new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }    
 }
